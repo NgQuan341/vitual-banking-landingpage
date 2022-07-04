7b13c04772bb296f983686fd5a7dc05e85d1d3ce
NgQuan341 <nghongquan34@gmail.com>
2022-07-04 17:37:45 +0700

    create: navbar element

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
+import { BrowserRouter as Router } from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <div className="App">
-      <h1>
-        hello
-      </h1>
+      <Router>
+        <Navbar/>
+      </Router>
     </div>
   )
 }
