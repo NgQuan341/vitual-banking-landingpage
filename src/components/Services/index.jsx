77507e00235d1ab0044b1dbbad5872dba27ecd51
NgQuan341 <nghongquan34@gmail.com>
2022-07-06 13:56:50 +0700

    create services section

---
+++ src/components/Services/index.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {svg4 as Icon1, svg2 as Icon2, svg3 as Icon3} from "../../images/ImageExport"
+import {
+  ServicesCard,
+  ServicesContainer,
+  ServicesH1,
+  ServicesH2,
+  ServicesIcon,
+  ServicesP,
+  ServicesWrapper,
+} from "./ServicesElements";
+
+const Services = () => {
+  return (
+    <>
+      <ServicesContainer>
+        <ServicesH1>Our Services</ServicesH1>
+        <ServicesWrapper>
+          <ServicesCard>
+            <ServicesIcon src={Icon1}/>
+            <ServicesH2>Reduce expenses</ServicesH2>
+            <ServicesP>
+              We help reduce your fess and increase your overall revenue
+            </ServicesP>
+          </ServicesCard>
+          <ServicesCard>
+            <ServicesIcon src={Icon2}/>
+            <ServicesH2>Virtual Offices</ServicesH2>
+            <ServicesP>
+              You can access out platform online anywhere in the world.
+            </ServicesP>
+          </ServicesCard>
+          <ServicesCard>
+            <ServicesIcon src={Icon3}/>
+            <ServicesH2>Premium Benefits</ServicesH2>
+            <ServicesP>
+              Unlock our special membership card that returns 5%
+            </ServicesP>
+          </ServicesCard>
+        </ServicesWrapper>
+      </ServicesContainer>
+    </>
+  );
+};
+
+export default Services;
--- src/images/ImageExport.js
+++ src/images/ImageExport.js
@@ -1,5 +1,6 @@
 import svg1 from './svg-1.svg'
 import svg2 from './svg-2.svg'
 import svg3 from './svg-3.svg'
+import svg4 from './svg-4.svg'
 
-export {svg1, svg2, svg3}
+export {svg1, svg2, svg3, svg4}
